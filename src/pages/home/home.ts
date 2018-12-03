07a5d5638754e33e7f3088830748f415d110e5c9
crystaljoy.tolentino@gmail.com <crystaljoy.tolentino@gmail.com>
2018-12-03 15:39:50 +0800

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,85 @@
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+
+@Component({
+  selector: "page-home",
+  templateUrl: "home.html"
+})
+export class HomePage {
+  userSelect: string = "custom";
+
+  //for 8 ball
+  isMoving: boolean = false;
+  isShowAnswer: boolean = false;
+
+  answer: string = "";
+  
+  answers: string[] = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes, definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+  ];
+
+  //for dice
+  number: number = 1;
+  isRolling: boolean = false;
+
+  //for custom
+  inputs: string= "";
+  inputArray: string[] = [];
+  output: string = "";
+  isRandomizing: boolean = false;
+
+  constructor(public navCtrl: NavController) {
+      setInterval(() => {if(this.isRolling) this.rollDice()}, 100);
+  }
+
+  rollDice(){
+    this.number = Math.floor(Math.random() * 6) + 1;
+  }
+
+  checkIfRolling(){
+    this.isRolling = !this.isRolling;
+  }
+
+  move(event: boolean, action: string) {
+    this.isMoving = event;
+
+    if (!this.isMoving && action == "show-answer") {
+      let index = Math.floor(Math.random() * 20) + 1;
+      this.answer = this.answers[index];
+      this.isShowAnswer = true;
+    }
+
+    if (!this.isMoving && action == "hide-answer") {
+      this.isShowAnswer = false;
+    }
+  }
+
+  randomize(){
+    this.inputArray = this.inputs.split(",");
+    let index = Math.floor(Math.random() * this.inputArray.length) + 1;
+    this.output = this.inputArray[index];
+  }
+
+  addToList(event: string){
+   this.inputs = event;
+  }
+}
